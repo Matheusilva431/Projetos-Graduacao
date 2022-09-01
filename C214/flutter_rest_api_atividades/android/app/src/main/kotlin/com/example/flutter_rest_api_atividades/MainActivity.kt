053b913a9199473cfd02593afffd519956b81102
Matheus Chagas da Silva <85804680+Matheusilva431@users.noreply.github.com>
2022-09-01 16:50:46 -0300

    Delete C214/flutter_rest_api_atividades directory

--- C214/flutter_rest_api_atividades/android/app/src/main/kotlin/com/example/flutter_rest_api_atividades/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_rest_api_atividades
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- C214/flutter_rest_api_atividades/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import '../view/home.dart';
-
-void main() {
-  runApp(const MaterialApp(home: Home()));
-}
